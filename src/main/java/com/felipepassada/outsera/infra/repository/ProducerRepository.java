5abf95115b969a3fc3a946244e923e44f457c3e0
fpassada <felipepassada@gmail.com>
2026-07-15 19:14:34 -0300

    ref: updated querie and database strategy

--- src/main/java/com/felipepassada/outsera/infra/repository/ProducerRepository.java
+++ src/main/java/com/felipepassada/outsera/infra/repository/ProducerRepository.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Repository
 public interface ProducerRepository extends JpaRepository<Producer, Long> {
 
-    @Query("SELECT DISTINCT p FROM Producer p INNER JOIN FETCH p.movies m WHERE m.winner = true")
+    @Query("SELECT DISTINCT p FROM Producer p JOIN FETCH p.movies m WHERE p.id IN " +
+            "(SELECT DISTINCT prod.id FROM Movie mov JOIN mov.producers prod WHERE mov.winner = true)")
     List<Producer> findAllWinnersWithMovies();
 }
